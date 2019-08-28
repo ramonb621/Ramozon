fc6733e31c962e42e4114ec2fc88594ab2312c60
Ramon Barajas <ramonb621@yahoo.com>
2019-08-27 17:15:00 -0700

    changed database file name and began to format js

---
+++ app.js
@@ -0,0 +1,14 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    user: "root",
+  
+    password: "",
+    database: "ramozon_db"
+  });
+  
---
+++ ramozon_db.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS ramzon_db;
+
+CREATE DATABASE ramzon_db;
+
+USE ramozon_db;
+
